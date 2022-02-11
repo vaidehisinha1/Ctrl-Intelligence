eae92bd58e61b0dcdbba682fe65785752d0f1ee1
ShreeBohara <90108789+ShreeBohara@users.noreply.github.com>
2022-02-11 18:48:38 +0530

    Add files via upload

---
+++ shreetest/lib/main.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'patient_info.dart';
+import 'hospital_info.dart';
+
+void main() {
+  runApp(const FormApp());
+}
+
+final demos = [
+  Demo(
+    name: 'Hospital Information',
+    route: '/hospital_info',
+    builder: (context) => const hospital_info(),
+  ),
+  Demo(
+    name: 'Patient Info',
+    route: '/autofill',
+    builder: (context) => const AutofillDemo(),
+  ),
+ 
+];
+
+class FormApp extends StatelessWidget {
+  const FormApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Form Samples',
+      theme: ThemeData(primarySwatch: Colors.teal),
+      routes: Map.fromEntries(demos.map((d) => MapEntry(d.route, d.builder))),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Form Samples'),
+      ),
+      body: ListView(
+        children: [...demos.map((d) => DemoTile(demo: d))],
+      ),
+    );
+  }
+}
+
+class DemoTile extends StatelessWidget {
+  final Demo? demo;
+
+  const DemoTile({this.demo, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(demo!.name),
+      onTap: () {
+        Navigator.pushNamed(context, demo!.route);
+      },
+    );
+  }
+}
+
+class Demo {
+  final String name;
+  final String route;
+  final WidgetBuilder builder;
+
+  const Demo({required this.name, required this.route, required this.builder});
+}
