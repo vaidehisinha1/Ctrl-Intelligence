002e5858419eecb15ebb754806b8c09975e9bae2
shravanicm17 <99279804+shravanicm17@users.noreply.github.com>
2022-02-13 10:10:01 +0530

    Update profile.dart

--- test/lib/pages/profile.dart
+++ test/lib/pages/profile.dart
@@ -12,6 +12,18 @@ class _ProfileState extends State<Profile> {
         appBar: new AppBar(
         title: new Text('PROFILE'),
     )
+      body: Padding(
+              padding: EdgeInsets.all(5.0),
+              child: Column(
+                children: [Text('\nKAMLA NEHRU HOSPITAL', textAlign: TextAlign.left,style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),),
+                  Text('Government Hospital, Pune, Maharashtra',textAlign: TextAlign.left, style: TextStyle(fontSize:20,fontWeight: FontWeight.bold)),
+                  Text('\nADDRESS:\n Mangalwar Peth Rd, near Kasba Post Office, Gandhinagar,Mangalwar Peth, Kasba Peth, Pune ,Maharashtra 411002',textAlign: TextAlign.center, style: TextStyle(fontSize:20)),
+                  Text('\nSTATUS: \nOpen 24 Hours ',textAlign: TextAlign.center,style: TextStyle(fontSize: 20,),),
+                  Text('\nCONTACT:\n 020 2550 8500',textAlign: TextAlign.center,style: TextStyle(fontSize: 20,),),
+                  Text('\nOTHER  INFO:\n COVID-19 vaccine available',textAlign: TextAlign.center,style: TextStyle(fontSize: 20,),)
+                ],
+              ),
+            )
     );
   }
 }
